bc098a458fcba96ed2dbc4129cf1674d8b021f6c
Anna Gruebler <contact@annagruebler.com>
2023-12-20 16:21:10 +0000

    update deploy

--- Retail/deploy.sh
+++ Retail/deploy.sh
@@ -6,11 +6,6 @@ echo "Bucket is $bucket"
 pwd
 
 
-echo "Preprocess the retail data"
-
-
-sleep 60
-
 echo "Starting the copy to S3 data"
 # aws s3 cp ./poc_data/users.csv s3://$bucket/train/users.csv
 # aws s3 cp ./poc_data/interactions.csv s3://$bucket/train/interactions.csv
@@ -20,5 +15,8 @@ echo "Starting the copy to S3 data"
 aws s3 cp s3://retail-demo-store-us-east-1/csvs/users.csv s3://$bucket/train/users.csv
 aws s3 cp s3://retail-demo-store-us-east-1/csvs/interactions.csv s3://$bucket/train/interactions.csv
 aws s3 cp s3://retail-demo-store-us-east-1/csvs/items.csv s3://$bucket/train/items.csv
+
+sleep 60
+
 aws s3 cp ./params.json s3://$bucket/train/params.json
 
